c6e6eadea112c7cc5e100d944515a958a138bdbd
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-23 18:48:28 +0530

    Added program 6

---
+++ assessment_1/program_6.c
@@ -0,0 +1,18 @@
+// Program 6: Find Remainder for two digit
+
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+
+    printf("Enter Number: ");
+    scanf("%d", &x);
+
+    y = x % 10;
+
+    printf("Result = %d", y);
+
+    return 0;
+}
+//
