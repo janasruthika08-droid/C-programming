69df1eb2669df79d332fc46f0151b6f9dedf7a87
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-23 21:24:30 +0530

    Added program 04

---
+++ assessment_2/program_04.c
@@ -0,0 +1,15 @@
+//Check number greater than 50
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    if(x>50){
+        printf("1");
+    }else{
+        printf("0");
+    }
+    return 0;
+}
+//
