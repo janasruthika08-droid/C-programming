e0a77cbcd2d9b7c342b4a0a3507e9349d47d19b2
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-23 21:35:01 +0530

    Added program 10

---
+++ assessment_2/program_10.c
@@ -0,0 +1,17 @@
+//Check zero digit greater than first
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    y=x%10;
+    z=x/10;
+    if(y>z){
+        printf("1");
+    }else{
+        printf("0");
+    }
+    return 0;
+}
+//
