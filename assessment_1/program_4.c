0c9f5bc3715ae77695eb805c1c24c7f4d83f133e
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-23 18:46:53 +0530

    Added program 4

---
+++ assessment_1/program_4.c
@@ -0,0 +1,18 @@
+// Program 4:Divide a Number by 6
+
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+
+    printf("Enter Number: ");
+    scanf("%d", &x);
+
+    y = x / 6;
+
+    printf("Result = %d", y);
+
+    return 0;
+}
+//
