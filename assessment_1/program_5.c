1d47d6b3d9638a25f9cde34a9c78899623f709a0
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-23 18:47:38 +0530

    Added program 5

---
+++ assessment_1/program_5.c
@@ -0,0 +1,18 @@
+// Program 5:Find Remainder when Divided by 8
+
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+
+    printf("Enter Number: ");
+    scanf("%d", &x);
+
+    y = x % 8;
+
+    printf("Result = %d", y);
+
+    return 0;
+}
+//
