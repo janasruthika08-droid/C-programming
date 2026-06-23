17e3a8e90be9c5ae3ce97c58938761c584d3590e
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-23 18:44:55 +0530

    Added program 3

---
+++ assessment_1/program_3.c
@@ -0,0 +1,18 @@
+// Program 3:Multiply a Number by 3
+
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+
+    printf("Enter Number: ");
+    scanf("%d", &x);
+
+    y = x * 3;
+
+    printf("Result = %d", y);
+
+    return 0;
+}
+//
