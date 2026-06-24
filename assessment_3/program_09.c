e0009b157f8e3b6f162e7964c6814a150ba157ae
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-24 09:30:33 +0530

    Added program 09

---
+++ assessment_3/program_09.c
@@ -0,0 +1,17 @@
+//Check digit less than 0
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    y=x%10;
+    z=x/10;
+    if(y<z){
+        printf("Success");
+    }else{
+        printf("Failure");
+    }
+    return 0;
+}
+//
