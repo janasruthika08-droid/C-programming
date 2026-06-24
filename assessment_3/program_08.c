c5d8c8266749551e88c55be18d8ad8b6164dced3
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-24 09:28:49 +0530

    Added program 08

---
+++ assessment_3/program_08.c
@@ -0,0 +1,17 @@
+//Check non-identical numbers
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    y=x%10;
+    z=x/10;
+    if(y!=z){
+        printf("Success");
+    }else{
+        printf("Failure");
+    }
+    return 0;
+}
+//
