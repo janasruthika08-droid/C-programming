64306a1025e7ae1eefb6798ac78c8ff214c200f7
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-24 09:35:52 +0530

    Added program 12

---
+++ assessment_3/program_12.c
@@ -0,0 +1,17 @@
+//Check first digit greater than or equal to zero digit
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    y=x%10;
+    z=x/10;
+    if(z>=y){
+        printf("Success");
+    }else{
+        printf("Failure");
+    }
+    return 0;
+}
+//
