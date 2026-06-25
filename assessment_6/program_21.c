df52a1ea5e366e14a7314e60b357b79a391fb127
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-25 11:34:42 +0530

    Added program 21

---
+++ assessment_6/program_21.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, digit;
+    int count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n > 0)
+    {
+        digit = n % 10;
+
+        if(digit % 2 != 0)
+        {
+            count++;
+        }
+
+        n = n / 10;
+    }
+
+    printf("Total odd digits = %d", count);
+
+    return 0;
+}
