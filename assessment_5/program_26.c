a6458492126f0b58e725f255fba1163f7a90d21b
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-25 09:47:02 +0530

    Added program 26

---
+++ assessment_5/program_26.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    for(int i = 9999; i >= 1000; i--)
+    {
+        if(i % 7 == 0 && i % 9 == 0)
+        {
+            printf("%d", i);
+            break;
+        }
+    }
+
+    return 0;
+}
