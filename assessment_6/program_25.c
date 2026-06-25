46e569cd9740bdeb5929ba3be03fa2e12edbf4ba
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-25 11:42:29 +0530

    Added program 25

---
+++ assessment_6/program_25.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, digit;
+    int count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n > 0)
+    {
+        digit = n % 10;
+
+        if(digit == 2 || digit == 3 || digit == 5 || digit == 7)
+        {
+            count++;
+        }
+
+        n = n / 10;
+    }
+
+    printf("Total single-digit prime numbers = %d", count);
+
+    return 0;
+}
