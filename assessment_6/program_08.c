02aade312d9b70b26782c2ed2db7fc0ea2495aae
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-25 11:05:40 +0530

    Added program 08

---
+++ assessment_6/program_08.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 11;
+    int ones,tens;
+
+    while(i <= 99)
+    {
+        ones=i%10;
+        tens=i/10;
+        if((i % 2 == 0) && (ones+tens==6))
+        {
+            printf("%d\n", i);
+        }
+        i++;
+    }
+
+    return 0;
+}
