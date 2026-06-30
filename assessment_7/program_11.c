8f450f478956210158c3cd728bedb8ff18f448fe
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-06-30 18:41:35 +0530

    Added program 11

---
+++ assessment_7/program_11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void count_total_digits(int);
+
+int main()
+{
+    int x;
+
+    scanf("%d", &x);
+    count_total_digits(x);
+
+    return 0;
+}
+
+void count_total_digits(int x)
+{
+    int count = 0;
+
+    while(x > 0)
+    {
+        count++;
+        x = x / 10;
+    }
+
+    printf("%d", count);
+}
