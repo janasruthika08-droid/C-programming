3ae369663c33f58e48d4e9245a8783b4820ed906
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-07-01 15:52:40 +0530

    Added program 16

---
+++ assessment_9/program_16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a[50], b[50], c[51];
+    int i, carry = 0, sum;
+
+    printf("Enter first 50-digit number:\n");
+    for(i = 0; i < 50; i++)
+        scanf("%1d", &a[i]);
+
+    printf("Enter second 50-digit number:\n");
+    for(i = 0; i < 50; i++)
+        scanf("%1d", &b[i]);
+
+    for(i = 49; i >= 0; i--) {
+        sum = a[i] + b[i] + carry;
+        c[i + 1] = sum % 10;
+        carry = sum / 10;
+    }
+
+    c[0] = carry;
+
+    printf("Result:\n");
+    for(i = 0; i < 51; i++)
+        printf("%d", c[i]);
+
+    return 0;
+}
