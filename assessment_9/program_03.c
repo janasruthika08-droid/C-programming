ade8b0124dbea86ab7bad8c8e27ff259667eb640
Janasruthika <janasruthika.bm23@bitsathy.ac.in>
2026-07-01 14:33:22 +0530

    Added program 03

---
+++ assessment_9/program_03.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5], i, min;
+
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    min = arr[0];
+
+    for(i = 1; i < 5; i++) {
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Smallest Number = %d", min);
+
+    return 0;
+}
